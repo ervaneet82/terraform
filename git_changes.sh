f2764951d671ef3214d8896cd366e0cd3f3c8e0c
Vaneet Gupta <vaneetgupta@Vaneets-MacBook-Pro.local>
2022-11-16 22:06:03 +0530

    modified testing

--- git_changes.sh
+++ git_changes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TOKEN="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"
+export TOKEN="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"
 /usr/bin/curl -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/ervaneet82/terraform/commits | jq '.files[].filename'
