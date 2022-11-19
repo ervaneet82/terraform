cd2b59092e69181288ce3795cd34416f6072cccb
Vaneet Gupta <vaneetgupta@Vaneets-MacBook-Pro.local>
2022-11-19 07:16:12 +0530

    modified testing

--- azure/main.tf
+++ azure/main.tf
@@ -5,7 +5,6 @@ resource "azurerm_virtual_machine" "test" {
   network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
   vm_size               = "Standard_DS1_v2"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
   # delete_os_disk_on_termination = true
 
 
