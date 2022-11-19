1b864fc37fd9b8ec80c6fda4babf9a2fd90cecb8
Vaneet Gupta <vaneetgupta@Vaneets-MacBook-Pro.local>
2022-11-19 07:27:12 +0530

    modified testing

--- git_changes.sh
+++ git_changes.sh
@@ -6,14 +6,20 @@ SHA2=`git log --pretty=oneline --abbrev-commit | head -2 | tail -1  | awk -F' ' 
 git diff --name-only $SHA2..$SHA1 > test.txt
 
 git clone https://$TOKEN@github.com/ervaneet82/devops.git
-for f in `cat test.txt`
+or f in `cat test.txt`
 do
   directory=`echo $f | awk -F'/' '{$NF=""; print $0}' | sed 's/ /\//g'`
   file=`echo $f | awk -F'/' '{print $NF}'`
   echo $directory
   echo $file
-  mkdir -p devops/$directory
-  cp $file devops/$directory/
+  if [ -d $directory ];then
+    mkdir -p devops/$directory
+    if [ ! -f $file ];then
+      cp $directory/$file devops/$directory/
+    else
+      cp $file devops/
+    fi
+  fi
 done
 cd devops
 git add .
